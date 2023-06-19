6f119a9b0270e85c5adb4206f23339a661188fed
Anjali Kedia <24kedia.anjali@gmail.com>
2023-06-19 14:05:16 +0530

    made responsive

--- src/container/Content.jsx
+++ src/container/Content.jsx
@@ -16,7 +16,7 @@ const Content = () => {
     }
   return (
     <div>
-        <div className='flex flex-row justify-start ml-10 mt-10 mr-6 '>
+        <div className='md:flex md:flex-row md:justify-start ml-10 mt-10 mr-6 '>
         <button type="button" class="text-white bg-[#1D1F25] hover:text-black hover:bg-[#FFBB29] text-opacity-60 font-medium rounded-full text-base px-5 py-2.5 mr-2 mb-2" onClick={() => setItems(content)}>All</button>
         <button type="button" class="text-white bg-[#1D1F25] hover:text-black hover:bg-[#FFBB29] text-opacity-60 font-medium rounded-full text-base px-5 py-2.5 mr-2 mb-2" onClick={() => filterItem('Technology')}>Technology</button>
         <button type="button" class="text-white bg-[#1D1F25] hover:text-black hover:bg-[#FFBB29] text-opacity-60 font-medium rounded-full text-base px-5 py-2.5 mr-2 mb-2" onClick={() => filterItem('Education')}>Education</button>
--- src/container/Navbar.jsx
+++ src/container/Navbar.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 import demo from '../images/DemoCompany.png'
 import demoComp from '../images/DemoCompany.svg'
+import img from '../images/img1.jpg'
 import {RiSettings3Fill} from 'react-icons/ri';
 import {LuBellDot} from 'react-icons/lu'
 
 const Navbar = () => {
+  
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
+
     
 <nav className="bg-[#1D1F25] border-b border-[#FFFFFF] border-opacity-10 dark:bg-[#1D1F25]">
   <div className='flex flex-wrap'>
@@ -16,7 +21,19 @@ const Navbar = () => {
       
       
   </a>
-  <ul className="flex flex-col ml-8 text-sm font-one p-4 md:p-0 mt-4 border text-[#FFFFFF]  border-gray-100   md:flex-row md:space-x-8 md:mt-0 md:border-0 ">
+  <button onClick={() => setIsOpen(!isOpen)}  type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 
+  rounded-lg md:hidden   dark:text-gray-400 " aria-controls="navbar-default" aria-expanded="false">
+      <svg className={`w-6 h-6 ${isOpen ? "hidden" : "block"}`} aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+      <svg
+           className={`fill-current h-3 w-3 ${isOpen ? "block" : "hidden"}`}
+           viewBox="0 0 20 20"
+           xmlns="http://www.w3.org/2000/svg"
+         >
+           <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
+         </svg>
+    </button>
+    <div className={`w-full md:block md:w-auto ${isOpen ? "block" : "hidden"}`} id="navbar-default">
+  <ul  className="flex flex-col md:ml-8 text-sm font-one p-4 md:p-0 mt-4 border text-[#FFFFFF]  border-gray-100   md:flex-row md:space-x-8 md:mt-0 md:border-0 ">
       <li>
         <a href="#" className="block py-2 pl-3 pr-4 text-white  hover:text-[#FFBB29] text-opacity-50 md:p-0 " >Home</a>
       </li>
@@ -30,17 +47,18 @@ const Navbar = () => {
         <a href="#" className="block py-2 pl-3 pr-4 text-white  hover:text-[#FFBB29] text-opacity-50 md:p-0 ">Pricing</a>
       </li>
       <li>
-        <a href="#" className="block py-2 pl-3 pr-4 text-[#FFBB29]  hover:text-[#FFBB29]  md:p-0 "aria-current="page">Blog</a>
+        <a href="#" className="block py-2 pl-3 pr-4 text-[#FFBB29]  hover:text-[#FFBB29]  md:p-0 " aria-current="page">Blog</a>
       </li>
     </ul>
     </div>
+    </div>
 
-  <div className='flex items-center ml-auto gap-12'>
+  <div className='md:flex hidden md:items-center md:ml-auto md:gap-12'>
   <RiSettings3Fill className='text-white text-opacity-50'/>
   <LuBellDot className='text-white text-opacity-50'/>
   <button type="button" className="flex mr-8 text-sm bg-gray-800 rounded-full md:mr-8 focus:ring-4 focus:ring-gray-300
    dark:focus:ring-gray-600" id="user-menu-button"  >
-        <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="user photo"/>
+        <img class="w-8 h-8 rounded-full" src={img} alt="user photo"/>
   </button>
   
   </div>
